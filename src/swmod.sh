8abb5c573af742bc7fff9068a7caeb7c045b4ab1
Oliver Schulz <oliver.schulz@tu-dortmund.de>
2011-04-27 18:22:14 +0200

    Updated copyright info

--- src/swmod.sh
+++ src/swmod.sh
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 Oliver Schulz <oliver.schulz@tu-dortmund.de>
+# Copyright (C) 2009-2011 Oliver Schulz <oliver.schulz@tu-dortmund.de>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
